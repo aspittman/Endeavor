8b66c7f6bd3fae822a60e07f4f663cb64ea7b2c8
aspittman <aspittman@live.com>
2020-09-16 19:03:32 -0600

    Master Detail Flow complete

--- app/src/main/java/com/affinityapps/endeavor/ui/master/UpdateFormActivity.java
+++ app/src/main/java/com/affinityapps/endeavor/ui/master/UpdateFormActivity.java
@@ -1,5 +1,6 @@
 package com.affinityapps.endeavor.ui.master;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -8,6 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.affinityapps.endeavor.R;
 import com.affinityapps.endeavor.databinding.ActivityUpdateFormBinding;
+
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_DATE;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_HOURS;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_ID;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_MILES;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_ORGANIZATION;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_PROJECT;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_PURCHASES;
+import static com.affinityapps.endeavor.ui.home.HomeFragment.EXTRA_TITLE;
 
 public class UpdateFormActivity extends AppCompatActivity {
 
@@ -20,6 +30,17 @@ public class UpdateFormActivity extends AppCompatActivity {
         setContentView(view);
 
         UpdateFormFragment fragment = new UpdateFormFragment();
+        Bundle bundle = new Bundle();
+        Intent intent = getIntent();
+        bundle.putString(EXTRA_ID, intent.getStringExtra(EXTRA_ID));
+        bundle.putString(EXTRA_TITLE, intent.getStringExtra(EXTRA_TITLE));
+        bundle.putString(EXTRA_ORGANIZATION, intent.getStringExtra(EXTRA_ORGANIZATION));
+        bundle.putString(EXTRA_PROJECT, intent.getStringExtra(EXTRA_PROJECT));
+        bundle.putString(EXTRA_DATE, intent.getStringExtra(EXTRA_DATE));
+        bundle.putString(EXTRA_HOURS, intent.getStringExtra(EXTRA_HOURS));
+        bundle.putString(EXTRA_MILES, intent.getStringExtra(EXTRA_MILES));
+        bundle.putString(EXTRA_PURCHASES, intent.getStringExtra(EXTRA_PURCHASES));
+        fragment.setArguments(bundle);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.item_detail_container, fragment)
                 .commit();
